83630883765e9ff186f317daa40fa893fa7b1fe4
Laurence Zanotti <lau_zanotti@hotmail.com>
2023-04-21 00:24:09 -0300

    feat: console refactor + employee controller

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,2 +1,46 @@
-package controller;public class EmployeeController {
+package controller;
+import model.EmployeeModel;
+
+import java.util.Scanner;
+
+public class EmployeeController {
+    private final Scanner scan;
+
+    public EmployeeController() {
+        this.scan = new Scanner(System.in);
+    }
+
+    public void signup() {
+        System.out.println("Signing up an employee\n");
+
+        System.out.print("Name: ");
+        String name = scan.nextLine();
+        System.out.print("Age: ");
+        int age = scan.nextInt();
+        System.out.print("Gender: ");
+        char gender = scan.next().charAt(0);
+        System.out.print("Designation: ");
+        String designation = scan.nextLine();
+        System.out.print("Department: ");
+        String department = scan.nextLine();
+        System.out.print("Salary: ");
+        float salary = scan.nextFloat();
+
+        EmployeeModel employee = new EmployeeModel(name, age, gender, designation, department, salary);
+        System.out.println(employee);
+
+        System.out.println("Employee " + employee + " registered!\n");
+    }
+    public void list() {
+        System.out.println("List of employees\n");
+    }
+    public void retrieveDetails() {
+        System.out.println("Employee details\n");
+    }
+    public void updateDetails() {
+        System.out.println("Update employee information\n");
+    }
+    public void delete() {
+        System.out.println("Delete an employee\n");
+    }
 }
--- src/view/Console.java
+++ src/view/Console.java
@@ -1,13 +1,17 @@
 package view;
+import controller.EmployeeController;
+
 import java.util.Scanner;
 
 public class Console {
     private boolean isListening = false;
-    private Scanner scan;
+    private final Scanner scan;
+    private final EmployeeController controller;
 
     public Console() {
         startListening();
         this.scan = new Scanner(System.in);
+        this.controller = new EmployeeController();
     }
 
     /**
@@ -51,29 +55,24 @@ public class Console {
         }
     }
 
+    /**
+     * Requests the appropriate controller to perform an action
+     *
+     * @param in Takes a String input as an option to select
+     *          an action with employees
+     */
     private void performAction(String in) {
         switch (in) {
-            case "0":
+            case "0" -> {
                 System.out.println("Thanks for using EmpManSys. Exiting...");
                 stopListening();
-                break;
-            case "1":
-                System.out.println("Signing up an employee");
-                break;
-            case "2":
-                System.out.println("List of employees");
-                break;
-            case "3":
-                System.out.println("Employee details");
-                break;
-            case "4":
-                System.out.println("Update employee information");
-                break;
-            case "5":
-                System.out.println("Delete an employee");
-                break;
-            default:
-                System.out.println(
+            }
+            case "1" -> controller.signup();
+            case "2" -> controller.list();
+            case "3" -> controller.retrieveDetails();
+            case "4" -> controller.updateDetails();
+            case "5" -> controller.delete();
+            default -> System.out.println(
                     """
                     The options are:
                     1 - Sign up an employee
@@ -82,8 +81,7 @@ public class Console {
                     4 - Update an employee information
                     5 - Delete an employee
                     """
-                );
-                break;
+            );
         }
     }
 
