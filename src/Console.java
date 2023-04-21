bf91cc23a0d314346ff2684a6e80388c5c2ddacb
Laurence Zanotti <lau_zanotti@hotmail.com>
2023-04-20 22:17:01 -0300

    feat: greetings output

--- src/Console.java
+++ src/Console.java
@@ -1,8 +1,31 @@
 public class Console {
-    public Console() {
-    }
+    public Console() {}
 
-    public void greetings() {
-        System.out.println("EmpManSys 0.0.0");
+
+    /**
+     * Displays the application description and usage
+     */
+    public static void greetings() {
+        System.out.println(
+        """
+        EmpManSys 0.0.0
+        Author: @LaurenceZanotti (https://github.com/LaurenceZanotti)
+        -------------------------------------------------------------
+        Welcome to Employee Manager System.
+        
+        The Employee Management System is a console-based application
+        that manages employees' records for a company. The application
+        will allow administrators to create, read, update, and delete
+        employee records.
+        
+        Enter a number below to perform an action:
+        
+        1 - Sign up an employee
+        2 - List employees
+        2 - Show an employee details
+        3 - Update an employee information
+        4 - Delete an employee
+        """
+        );
     }
 }
